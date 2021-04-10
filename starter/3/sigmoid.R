ed04e18e0ee285f78d131886a86923dfefc1f635
Farid Cheraghi <faridcher@gmail.com>
2021-04-11 02:25:16 +0430

    ex3sigmoid#11

--- starter/3/sigmoid.R
+++ starter/3/sigmoid.R
@@ -9,6 +9,7 @@ sigmoid <- function(z) {
   # Instructions: Compute the sigmoid of each value of z (z can be a matrix,
   #               vector or scalar).
   
+  g <- 1 / (1 + exp(-1 * z))
   g
   # --------------------------------------------------------------------------
 }
